dbbb98814c5b3dd71791839a095414f9dca5e6f1
Kohara <1k8aioon@gmail.com>
2023-01-22 09:43:35 +0900

    commit
    audio start

---
+++ sound/PlaySound_test.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+playsound('C:/Users/kxiyt/Documents/GitHub/Postal-Disinfection/sound/0338.wav')
+
+
+
--- 音声再生/PlaySound_test.py
+++
@@ -1,5 +0,0 @@
-from playsound import playsound
-
-playsound("音声再生/sample_sound.mp3")
-playsound("C:\Users\kxiyt\Documents\GitHub\Postal-Disinfection\音声再生\sample_cound.mp3")
-
