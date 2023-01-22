0b4d945fb7e707f5cfd5aae6167b6c565bbd5e5b
aimlinux <1k8aioon@gmail.com>
2023-01-22 10:38:58 +0900

    commt

--- sound/PlaySound_test.py
+++ sound/PlaySound_test.py
@@ -1,6 +1,6 @@
 from playsound import playsound
 
-playsound('C:/Users/kxiyt/Documents/GitHub/Postal-Disinfection/sound/0338.wav')
+playsound('/home/pi/Documents/GitHub/Postal-Disinfection/sound/0338.wav')
 
 
 
